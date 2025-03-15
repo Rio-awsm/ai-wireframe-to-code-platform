84ffbd560fbb5d35da6ae8b0a5ab1ff948c45285
Rio-awsm <mm.saha1982@gmail.com>
2025-03-15 21:32:33 +0530

    initialize project structure with configuration files, components, and utilities

---
+++ app/(routes)/dashboard/_components/ImageUpload.tsx
@@ -0,0 +1,112 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Constants from "@/data/Contants"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { CloudUpload, WandSparkles, X } from "lucide-react";
+import Image from "next/image";
+import React, { ChangeEvent, useState } from "react";
+
+const ImageUpload = () => {
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [model, setModel] = useState<string>();
+  const onImageSelect = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const imageUrl = URL.createObjectURL(files[0]);
+      setPreviewUrl(imageUrl);
+    }
+  };
+  return (
+    <div className="mt-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        {!previewUrl ? (
+          <div className="p-7 border border-dashed rounded-md shadow-md flex flex-col items-center justify-center">
+            <CloudUpload className="h-10 w-10 text-primary" />
+            <h2 className="font-bold text-lg">Upload Image</h2>
+
+            <p className="text-gray-400 mt-3">
+              Click Button to Select Wireframe Image
+            </p>
+            <div className="p-5 border border-dashed w-full flex justify-center mt-7">
+              <label htmlFor="image-select">
+                <h2 className="p-2 bg-blue-300 text-primary font-medium rounded-md px-5">
+                  Select Image
+                </h2>
+              </label>
+            </div>
+            <input
+              type="file"
+              id="image-select"
+              className="hidden"
+              onChange={onImageSelect}
+              multiple={false}
+            />
+          </div>
+        ) : (
+          <div className="p-5 border border-dashed">
+            <Image
+              src={previewUrl}
+              alt="preview"
+              width={500}
+              height={500}
+              className="w-full h-[300px] object-contain"
+            />
+            <X
+              className="flex justify-end w-full cursor-pointer"
+              onClick={() => setPreviewUrl(null)}
+            />
+          </div>
+        )}
+
+        <div className="p-7 border shadow-md rounded-lg">
+          <h2 className="font-bold text-lg">Select AI Model</h2>
+          <Select onValueChange={(value) => setModel(value)}>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select AI Model" />
+            </SelectTrigger>
+            <SelectContent>
+              {Constants?.AiModelList.map((model, index) => (
+                <SelectItem value={model.name} key={index}>
+                  <div className="flex items-center gap-2">
+                    <Image
+                      src={model.icon}
+                      alt={model.name}
+                      width={25}
+                      height={25}
+                    />
+                    <h2> {model.name}</h2>
+                  </div>
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+
+          <h2 className="font-bold text-lg">
+            Enter description about your Webpage{" "}
+          </h2>
+          <Textarea
+            className="mt-3 h-[200px]"
+            placeholder="Write about your webpage"
+          />
+        </div>
+      </div>
+
+      <div className="mt-10 flex items-center justify-center">
+        <Button>
+          {" "}
+          <WandSparkles />
+          Convert to Code
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ImageUpload;
